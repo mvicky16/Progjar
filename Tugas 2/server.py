40ecbfdbf0a1b93971db8632962b140e4ad5dfc9
mvicky16 <vickyr.muhammad@gmail.com>
2019-03-27 06:48:50 +0700

    done

--- Tugas 2/server.py
+++ Tugas 2/server.py
@@ -1,24 +1,37 @@
 import socket
 import time
+import os
+import shutil
 import sys
+from threading import Thread
 
 IP = "127.0.0.1"
 PORT = 9000
-buf = 1024
-fn = 'A.jpg'
+fn = ['A.jpg', 'B.jpg','C.jpg']
 
+def req():
+	data, addr = sock.recvfrom(1024)
+	temp = str(data)
+	if data == "sambung":
+		sock.sendto("client",addr)
+		thread = Thread(target=sendImg, args=(addr))
+		thread.start()
+
+def sendImg(ip, port):
+	addr = (ip, port)
+	for x in fn:
+		print "Sending %s ..." % x
+		sock.sendto("masuk", addr)
+		sock.sendto(x,addr)
+		f = open(x, "rb")
+		data = f.read()
+		sock.sendto(data, addr)
+		time.sleep(0.02)
+	print " "
+	sock.sendto("done",addr)
 
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 sock.bind((IP, PORT))
 
-print "Sending %s ..." % fn
-
-f = open(fn, "r")
-n = f.read(buf)
-while(n):
-    if(sock.sendto(n, (IP, PORT))):
-        n = f.read(buf)
-        time.sleep(0.02)
-
-sock.close()
-f.close()
+while True:
+		req()

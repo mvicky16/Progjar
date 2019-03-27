9b09d1445416ecd42bb45ed50e4901af000e387c
mvicky16 <vickyr.muhammad@gmail.com>
2019-03-27 13:24:52 +0700

    -

--- Tugas 3/client.py
+++ Tugas 3/client.py
@@ -1,26 +1,23 @@
 import socket
-import time
 import sys
 import os
-
 
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 dn = 'Client1/%s'
 server_address = ('localhost', 10000)
 sock.connect(server_address)
-sock.sendall('sambung')
+sock.send('sambung')
 
 while True:
-	a, addr = sock.recv(1024)
+	a = sock.recv(1024)
 	if a == "done":
 		break
 	if a == ('masuk'):
-		namafile, addr = sock.recv(1024)
+		namafile = sock.recv(1024)
 		f = open(dn % namafile, 'wb+')
-		b, addr = sock.recv(20480)
+		b = sock.recv(20480)
 		f.write(b)
 		f.close()
 		print "File "+ namafile+ " telah diterima"
 		
 sock.close()
-
--- Tugas 3/server.py
+++ Tugas 3/server.py
@@ -9,34 +9,32 @@ server_address = ("localhost", 10000)
 fn = ['A.jpg', 'B.jpg','D.jpg']
 
 def req():
-	data, addr = connection.recv(1024)
 	temp = str(data)
 	if data == "sambung":
-		thread = Thread(target=sendImg, args=(addr))
+		thread = Thread(target=sendImg, args=(connection,))
 		thread.start()
 
 def sendImg(server_address):
 	#addr = (server_address)
 	for x in fn:
 		print "Mengirim File %s" % x
-		sock.sendall("masuk")
-		sock.sendall(x)
+		connection.sendall("masuk")
+		connection.sendall(x)
 		f = open(x, "rb")
 		data = f.read()
-		sock.sendall(data)
+		connection.sendall(data)
 		time.sleep(0.02)
 	print "----------------------------"
-	sock.sendall("done",addr)
+	connection.sendall("done")
 
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 sock.bind(server_address)
 
 sock.listen(1)
-while True:
-	#waiting connection
-	print >> sys.stderr, "waiting connection"
-	connection, client_address = sock.accept()
-	print >> sys.stderr, "connection from", client_address
+print >> sys.stderr, "waiting connection"
+connection, client_address = sock.accept()
+print >> sys.stderr, "connection from", client_address
 	
 while True:
+		data = connection.recv(1024)
 		req()
